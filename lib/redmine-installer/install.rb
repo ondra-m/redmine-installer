fba96b9e538af16013dd770caed1e0511d4f58cc
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2014-10-19 14:14:34 +0200

    installing with redmine

--- lib/redmine-installer/install.rb
+++ lib/redmine-installer/install.rb
@@ -7,6 +7,13 @@ module Redmine::Installer
       Redmine::Installer::Step::LoadPackage
     ]
 
+    attr_accessor :redmine
+
+    def initialize(redmine, options={})
+      self.redmine = redmine
+      super(options)
+    end
+
     def run
       @steps.each do |id, step|
         step.print_title
