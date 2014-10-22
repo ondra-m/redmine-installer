26fe23fffd34621b5bca9a3f69600233970210e7
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2014-10-22 13:24:57 +0200

    fix install non existing plugin

--- lib/redmine-installer/plugins/redmine_plugin.rb
+++ lib/redmine-installer/plugins/redmine_plugin.rb
@@ -1,12 +1,24 @@
 module Redmine::Installer::Plugin
   class RedminePlugin < Base
 
+    # Search plugins folder
+    def self.am_i_there?
+      records = Dir.glob(File.join('plugins', '*'))
+      records.map! do |record|
+        File.basename(record)
+      end
+
+      records.include?(self.class_name.downcase)
+    end
+
     class EasyProject < RedminePlugin
 
       RAKE_EASYPROJECT_INSTALL = 'bundle exec rake easyproject:install RAILS_ENV=production'
 
       def self.install
-        run_command(RAKE_EASYPROJECT_INSTALL, t('plugin.redmine_plugin.easyproject.install'))
+        if am_i_there?
+          run_command(RAKE_EASYPROJECT_INSTALL, t('plugin.redmine_plugin.easyproject.install'))
+        end
       end
     end
 
