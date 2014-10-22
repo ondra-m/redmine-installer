9a3fd209118ef3b71a28bcb31c6ad82f1d7caa11
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2014-10-22 18:58:09 +0200

    fix for hidden files

--- lib/redmine-installer/steps/move_redmine.rb
+++ lib/redmine-installer/steps/move_redmine.rb
@@ -5,10 +5,11 @@ module Redmine::Installer::Step
 
     def up
       # Move all files from tmp_redmine_root to redmine_root
-      FileUtils.mv(
-        Dir.glob(File.join(base.tmp_redmine_root, '*')),
-        base.redmine_root
-      )
+      Dir.glob(File.join(base.tmp_redmine_root, '{*,.*}')) do |entry|
+        next if entry.end_with?('.') || entry.end_with?('..')
+
+        FileUtils.mv(entry, base.redmine_root)
+      end
 
       # Delete tmp_redmine_root
       FileUtils.remove_entry_secure(base.settings[:tmpdir])
