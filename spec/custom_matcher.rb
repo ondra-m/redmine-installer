484f49c6c48fe75710ea6ac830881fad6ac7d9cd
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2016-07-18 10:36:26 +0200

    matcher wait N second

--- spec/custom_matcher.rb
+++ spec/custom_matcher.rb
@@ -2,12 +2,20 @@ require 'rspec/expectations'
 
 RSpec::Matchers.define :have_output do |text|
   match do |process|
-    x=process.get(text)
-    # binding.pry unless $__binding
-    expect(x).to include(text)
+    expect(process.get(text)).to include(text)
   end
 
   failure_message do |process|
     "expected that \"#{process.last_get_return}\" would contains: #{text}"
+  end
+end
+
+RSpec::Matchers.define :have_output_in do |text, second|
+  match do |process|
+    expect(process.get(text, max_wait: second)).to include(text)
+  end
+
+  failure_message do |process|
+    "expected that \"#{process.last_get_return}\" would contains: #{text} in #{second}s"
   end
 end
